af3d54aa02e0ca25ad7e97e93e26885265790d82
Josh Hagel <joshhagel@linchpinstudios.com>
2013-08-06 13:35:26 -0700

    Added Compressed JS to footer file

--- includes/footer.php
+++ includes/footer.php
@@ -2,6 +2,8 @@
       <div class="clearfix"></div>
     </div>
   </div>
+	
+	<script type="text/javascript" src="<?php echo($this->getThemePath().'/js/compressed.js'); ?>"></script>
 	<? Loader::element('footer_required'); ?>
  </body>
 </html>
